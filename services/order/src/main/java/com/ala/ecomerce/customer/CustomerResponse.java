cb037a2ec3880f51bf9caa34cedbfdd579013b74
bjaouiala <bjaouiala3@gmail.com>
2024-07-24 17:43:38 +0100

    make Http request between customer and order service using openFeign

--- services/order/src/main/java/com/ala/ecomerce/customer/CustomerResponse.java
+++ services/order/src/main/java/com/ala/ecomerce/customer/CustomerResponse.java
@@ -1,2 +1,9 @@
-package com.ala.ecomerce.customer;public record CustomerResponse() {
+package com.ala.ecomerce.customer;
+
+public record CustomerResponse(
+        String id,
+        String firstName,
+        String lastName,
+        String email
+) {
 }
