718feae66dc0a87de7b87180ad4e6503f0da4c3c
bjaouiala <bjaouiala3@gmail.com>
2024-07-30 14:53:53 +0100

    config java mailer sender to send notification

--- services/notification/src/main/java/com/ala/ecommerce/email/EmailService.java
+++ services/notification/src/main/java/com/ala/ecommerce/email/EmailService.java
@@ -1,12 +1,93 @@
 package com.ala.ecommerce.email;
 
+import com.ala.ecommerce.kafka.order.Product;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
-    private final JavaMailSender mailSender;
-    private final SpringTe
+    private final JavaMailSender mailSender ;
+    private final SpringTemplateEngine templateEngine;
+
+    @Async
+    public void sendPaymentSuccessEmail(
+            String destinationEmail,
+            String customerName,
+            BigDecimal amount,
+            String orderReference
+    ) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage,MimeMessageHelper.MULTIPART_MODE_RELATED, StandardCharsets.UTF_8.name());
+
+        messageHelper.setFrom("bjaouiala3@gmail.com");
+        final String templateName = EmailTemplate.PAYMENT_CONFIRMATION.getTemplate();
+        Map<String,Object> variables = new HashMap<>();
+        variables.put("customerName",customerName);
+        variables.put("amount",amount);
+        variables.put("orderReference",orderReference);
+        Context context = new Context();
+        context.setVariables(variables);
+        messageHelper.setSubject(EmailTemplate.PAYMENT_CONFIRMATION.getSubject());
+        try {
+            String htmlTemplate =templateEngine.process(templateName,context);
+            messageHelper.setText(htmlTemplate,true);
+            messageHelper.setTo(destinationEmail);
+            mailSender.send(mimeMessage);
+            log.info(String.format("email successfully sent to %s",destinationEmail));
+        }catch (MessagingException e){
+            log.warn(String.format("WARNNING, cannot send the emal to %s",destinationEmail));
+        }
+
+    }
+
+
+    @Async
+    public void sendOrderConfirmationEmail(
+            String destinationEmail,
+            String customerName,
+            BigDecimal amount,
+            String orderReference,
+            List<Product> products
+    ) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage,MimeMessageHelper.MULTIPART_MODE_RELATED, StandardCharsets.UTF_8.name());
+
+        messageHelper.setFrom("bjaouiala3@gmail.com");
+        final String templateName = EmailTemplate.ORDER_CONFIRMATION.getTemplate();
+        Map<String,Object> variables = new HashMap<>();
+        variables.put("customerName",customerName);
+        variables.put("totaleAmount",amount);
+        variables.put("orderReference",orderReference);
+        variables.put("products",products);
+        Context context = new Context();
+        context.setVariables(variables);
+        messageHelper.setSubject(EmailTemplate.ORDER_CONFIRMATION.getSubject());
+        try {
+            String htmlTemplate =templateEngine.process(templateName,context);
+            messageHelper.setText(htmlTemplate,true);
+            messageHelper.setTo(destinationEmail);
+            mailSender.send(mimeMessage);
+            log.info(String.format("email successfully sent to %s",destinationEmail));
+        }catch (MessagingException e){
+            log.warn(String.format("WARNNING, cannot send the emal to %s",destinationEmail));
+        }
+
+    }
+
 }
--- services/notification/src/main/java/com/ala/ecommerce/email/EmailTemplate.java
+++ services/notification/src/main/java/com/ala/ecommerce/email/EmailTemplate.java
@@ -1,2 +1,18 @@
-package com.ala.ecommerce.email;public enum EmailTemplate {
+package com.ala.ecommerce.email;
+
+import lombok.Getter;
+
+public enum EmailTemplate {
+    PAYMENT_CONFIRMATION("payment-confirmation.html","payment successfully processed"),
+    ORDER_CONFIRMATION("order-confirmation.html","order successfully processed");
+
+    @Getter
+    private final String template;
+    @Getter
+    private final String subject;
+
+    EmailTemplate(String template, String subject) {
+        this.template = template;
+        this.subject = subject;
+    }
 }
